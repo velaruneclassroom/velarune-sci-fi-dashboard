45cc84d593ae9894ea3fe92a7dd7f1c2a6fac5cf
velaruneclassroom <aking1@tps501.org>
2025-07-13 15:24:27 -0500

    Delete Velarune-SciFi-Dashboard directory

--- Velarune-SciFi-Dashboard/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC0T9oajBB3jhPMjshdIbjfpssTllFjjTI",
-  authDomain: "velarune-rpg.firebaseapp.com",
-  projectId: "velarune-rpg",
-  storageBucket: "velarune-rpg.appspot.com",
-  messagingSenderId: "832434090158",
-  appId: "1:832434090158:web:312f1e71dd830d8c1fd3f"
-};
-firebase.initializeApp(firebaseConfig);
