0a8a754f7b4ad3090d2947e35702141ca1bc672f
velaruneclassroom <aking1@tps501.org>
2025-07-13 15:36:47 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyC0T9oajBB3jhPMjshdIbjfpssTllFjjTI",
+  apiKey: "AIzaSyCOT9oajBB3jIhPMjshdIbjfpssTllFijI",
   authDomain: "velarune-rpg.firebaseapp.com",
   projectId: "velarune-rpg",
-  storageBucket: "velarune-rpg.appspot.com",
+  storageBucket: "velarune-rpg.firebasestorage.app",
   messagingSenderId: "832434090158",
-  appId: "1:832434090158:web:312f1e71dd830d8c1fd3f"
+  appId: "1:832434090158:web:312f11e71dd830d8c1fd3f"
 };
 firebase.initializeApp(firebaseConfig);
